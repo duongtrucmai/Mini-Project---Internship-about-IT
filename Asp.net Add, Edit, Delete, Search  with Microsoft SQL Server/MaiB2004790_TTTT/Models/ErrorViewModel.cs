731a6000b8c52c853f6409fceeb63cced41b62e7
Dương Trúc Mai <95559754+duongtrucmai@users.noreply.github.com>
2024-06-21 18:43:20 +0700

    Delete Asp.net Add, Edit, Delete, Search  with Microsoft SQL Server directory

--- Asp.net Add, Edit, Delete, Search  with Microsoft SQL Server/MaiB2004790_TTTT/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MaiB2004790_TTTT.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
